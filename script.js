6bec6eaff3e406a4e1f8979a44a3afb038a64950
OpaliteArchitect <179358946+OpaliteArchitect@users.noreply.github.com>
2026-09-17 20:32:17 +0800

    add matrix manipulation functions

--- script.js
+++ script.js
@@ -19,3 +19,47 @@ function indexToCol(index) {
 function indexToRow(index) {
     return index / 4;
 }
+
+function rotateBoardClockwise(rotations) {
+    for (let i = 0; i < rotations; i++) {
+        for (let r = 0; r < 4; r++) {
+            for (let c = r + 1; c < 4; c++) {
+                [board[r][c], board[c][r]] = [board[c][r], board[r][c]];
+            }
+        }
+
+        for (let r = 0; r < 4; r++) {
+            [board[r][0], board[r][3]] = [board[r][3], board[r][0]];
+            [board[r][1], board[r][2]] = [board[r][2], board[r][1]];
+        }
+    }
+}
+
+function shiftBoardLeft() {
+    for (const row of board) {
+        for (let c = 1; c < row.length; c++) {
+            if (row[c - 1] === 0) {
+                row[c - 1] = row[c];
+                row[c] = 0;
+            }
+        }
+    }
+
+    for (const row of board) {
+        for (let c = 0; c < row.length - 1; c++) {
+            if (row[c] !== 0 && row[c] === row[c + 1]) {
+                row[c] = row[c] * 2;
+                row[c + 1] = 0;
+            }
+        }
+    }
+
+    for (const row of board) {
+        for (let c = 1; c < row.length; c++) {
+            if (row[c - 1] === 0) {
+                row[c - 1] = row[c];
+                row[c] = 0;
+            }
+        }
+    }
+}
