2e559da86547dafeb6262907f17508eaf9a8716d
OpaliteArchitect <179358946+OpaliteArchitect@users.noreply.github.com>
2026-09-19 11:10:09 +0800

    add tile colors

--- js/controller.js
+++ js/controller.js
@@ -8,10 +8,11 @@ import {
     status,
 } from "./game-model.js";
 
-let highScore = Number(localStorage.getItem("highScore")) ?? 0;
+let highScore = Number(localStorage.getItem("highScore") ?? 0);
 const WINNING_MESSAGE = "You won!";
 const LOSING_MESSAGE = "Game over.";
 
+//mock();
 resetGame();
 
 function resetGame() {
@@ -20,6 +21,18 @@ function resetGame() {
     resetBoard();
     spawnNewTile();
     spawnNewTile();
+    render();
+}
+
+function mock() {
+    let value = 2;
+    for (let i = 0; i < 16; i++) {
+        const row = indexToRow(i);
+        const col = indexToCol(i);
+
+        board[row][col] = value;
+        value *= 2;
+    }
     render();
 }
 
@@ -74,6 +87,15 @@ function render() {
     for (let i = 0; i < dom.cells.length; i++) {
         const value = board[indexToRow(i)][indexToCol(i)];
         dom.cells[i].textContent = value === 0 ? "" : value;
+
+        const power = Math.log2(value);
+        const hue = Math.max(0, 60 - (power - 1) * 5);
+        const saturation = Math.max(100, 50 + (power - 1) * 5);
+        const lightness = Math.max(45, 95 - (power - 1) * 5);
+        dom.cells[i].style.setProperty(
+            "background-color",
+            value ? `hsl(${hue} ${saturation}% ${lightness}%)` : "white",
+        );
     }
 
     dom.score.textContent = status.score;
